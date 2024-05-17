30749ecff868f50f023eb58429aa0b0716084725
Raman Prasad <ramanprasad.0203@gmail.com>
2024-05-17 12:11:17 +0530

    minor changes

--- utility/db.js
+++ utility/db.js
@@ -4,9 +4,9 @@ const { Client } = require("pg");
 const connectionString =
   "postgres://postgres.kaajgksvthactygyupbx:Wm3Ohw95H2EFKcpV@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres" ||
   process.env.dbstring;
-
+const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthYWpna3N2dGhhY3R5Z3l1cGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2Mjk2MDAsImV4cCI6MjAzMTIwNTYwMH0.i5JFfHIK4lswz1m56wsW0bG9pN6XEYH7aCJ6s0kqWU4"
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.supabseurl, process.env.supabsekey);
+const supabase = createClient(process.env.supabseurl, process.env.supabsekey||supabaseKey);
 
 const connection = async () => {
   return await new Client({ connectionString });
